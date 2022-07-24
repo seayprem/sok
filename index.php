82520f85e7259f4d3b87666c2411049f4e15a400
seayprem <seayprem@hotmail.com>
2022-07-24 15:54:37 +0700

    Fixed alert normal to sweetalert2

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php 
 session_start();
+error_reporting(0);
 if(empty($_SESSION['emp_level'])) {
-  echo '<script>alert("คุณไม่ได้รับอนุญาตในการเข้าถึงหน้าต่างนี้");window.location.href = "login.php"</script>';
+  echo '<script src="js/sweetalert2@11.js"></script>';
+  echo '<script src="js/jquery-3.6.0.min.js"></script>';
+  echo "<script>
+  $(document).ready(function() {
+    $('div').hide();
+    Swal.fire({
+      icon: 'error',
+      title: 'คุณไม่ได้รับอนุญาตในการเข้าถึงหน้าต่างนี้',
+      text: 'กรุณาเข้าสู่ระบบ!',
+    }).then((result) => {
+      window.location.href = 'login.php';
+    });
+  });
+  </script>";
+  // echo '<script>alert("คุณไม่ได้รับอนุญาตในการเข้าถึงหน้าต่างนี้");window.location.href = "login.php"</script>';
 }
 ?>
 
