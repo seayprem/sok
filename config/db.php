5cc5bd16e1fe0d2845f642a3f5b68633d02230c8
seayprem <seayprem@hotmail.com>
2022-07-24 15:52:10 +0700

    Fixed Format Time

--- config/db.php
+++ config/db.php
@@ -6,6 +6,19 @@ $DB_NAME = "sok";
 
 $conn = mysqli_connect($DB_SERVER, $DB_USER, $DB_PASS, $DB_NAME);
 
+function DateThai($strDate) {
+  $strYear = date("Y",strtotime($strDate))+543;
+  $strMonth= date("n",strtotime($strDate));
+  $strDay= date("j",strtotime($strDate));
+  $strHour= date("H",strtotime($strDate));
+  $strMinute= date("i",strtotime($strDate));
+  $strSeconds= date("s",strtotime($strDate));
+  $strMonthCut = Array("","ม.ค.","ก.พ.","มี.ค.","เม.ย.","พ.ค.","มิ.ย.","ก.ค.","ส.ค.","ก.ย.","ต.ค.","พ.ย.","ธ.ค.");
+  $strMonthThai=$strMonthCut[$strMonth];
+  return "$strDay $strMonthThai $strYear, $strHour:$strMinute";
+
+}
+
 if(!$conn) {
   echo "error";
 }
