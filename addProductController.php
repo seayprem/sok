23ecd50b8f438d50182f0f3e5bc79a739db383e7
PuPeepPuPeep <cjsonastv@gmail.com>
2022-08-01 02:04:30 +0700

    Change addproduct if size=XL will add L

--- addProductController.php
+++ addProductController.php
@@ -15,13 +15,25 @@ if(isset($_POST['addproduct'])) {
   $code_sub = $_POST['code'].'L';
   $name_sub = $_POST['name'].' เหลือ';
 
-  $sql = "INSERT INTO `inventory` (inv_id, inv_name, inv_qty, inv_min, inv_max, inv_size, inv_color, cate_id, inv_sub_id) VALUES ('".$code."', '".$name."', $qty, $min, $max, '$size', '$color', $type, null), ('".$code_sub."', '".$name_sub."', 0, 0, 0, 'L', '$color', $type, '".$code."')";
-  $query = mysqli_query($conn, $sql);
-  if($query) {
-    echo "success";
-  } else {
-    echo "failed";
+  if($size == 'XL') {
+    $sql = "INSERT INTO `inventory` (inv_id, inv_name, inv_qty, inv_min, inv_max, inv_size, inv_color, cate_id, inv_sub_id) VALUES ('".$code."', '".$name."', $qty, $min, $max, '$size', '$color', $type, null), ('".$code_sub."', '".$name_sub."', 0, 0, 0, 'L', '$color', $type, '".$code."')";
+    $query = mysqli_query($conn, $sql);
+    if($query) {
+      echo "success";
+    } else {
+      echo "failed";
+    }
   }
+  else {
+    $sql = "INSERT INTO `inventory` (inv_id, inv_name, inv_qty, inv_min, inv_max, inv_size, inv_color, cate_id) VALUES ('".$code."', '".$name."', $qty, $min, $max, '$size', '$color', $type)";
+    $query = mysqli_query($conn, $sql);
+    if($query) {
+      echo "success";
+    } else {
+      echo "failed";
+    }
+  }
+  
 
 }
 
