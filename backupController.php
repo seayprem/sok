adf6fd121c906d5c636f87a0e515734bf5d69b3a
SeayPrem <seayprem@hotmail.com>
2022-10-02 22:01:33 +0700

    fixed | name file export backup

--- backupController.php
+++ backupController.php
@@ -70,7 +70,7 @@
 	   
 	    header('Content-Description: File Transfer');
 	    header('Content-Type: application/octet-stream');
-	    header('Content-Disposition: attachment; filename=' . basename(date("Y-M-D_H:i:s") . $backup_file_name));
+	    header('Content-Disposition: attachment; filename=' . basename(date('Y:M:D_h:i:s'). '_' .$backup_file_name));
 	    header('Content-Transfer-Encoding: binary');
 	    header('Expires: 0');
 	    header('Cache-Control: must-revalidate');
